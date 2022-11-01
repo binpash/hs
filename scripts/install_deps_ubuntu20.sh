f08e4cca008c762004b2a995c421cd17e1e8f27c
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-11-01 11:46:09 -0400

    Add a mini script that installs riker (to run on cloudlab machines)

---
+++ scripts/install_deps_ubuntu20.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Install Riker's dependencies
+sudo apt install -y make clang llvm git gcc python3-cram file graphviz
+sudo update-alternatives --install /usr/bin/cram cram /usr/bin/cram3 100
+
+## Install Riker
+(cd deps/riker;  git submodule update --init --recursive; make; sudo make install)

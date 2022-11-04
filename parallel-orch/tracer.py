7d87e8c501b99ee3436ee1bba78c7ec35bae12f3
George Liargkovas <gliargovas@aueb.gr>
2022-11-04 14:11:33 -0400

    Redirect Rikerfile execution output to /dev/null

--- parallel-orch/tracer.py
+++ parallel-orch/tracer.py
@@ -1,10 +1,9 @@
 import subprocess
 
 def run_and_trace_workset(workset, trace_file):
-    print("=" * 61)
     write_cmds_to_rikerfile(workset)
     ## Call Riker to execute the remaining commands all in parallel
-    subprocess.run(["rkr", "--show"])
+    subprocess.run(["rkr", "--show"], stdout=subprocess.DEVNULL)
     ## Call Riker to get the trace
     ## TODO: Normally we would like to plug in Riker and get the actual Trace data structure
     subprocess.run(["rkr", "trace", "-o", trace_file])

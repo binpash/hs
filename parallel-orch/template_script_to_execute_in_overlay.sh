f3f15a33b781011af97030eee5b442ec157f9091
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-12-09 08:08:41 -0500

    fix riker working inside the sandbox (unshare+overlay)

--- parallel-orch/template_script_to_execute_in_overlay.sh
+++ parallel-orch/template_script_to_execute_in_overlay.sh
@@ -4,12 +4,18 @@
 echo $CMD_STRING > ./Rikerfile
 
 ## Call Riker to execute the command
-rkr --debug --log all --show
+# rkr --debug --log all --show
+rkr --show
 ## TODO: Run with gdb for debugging
 ## ```sh
-## gdb rkr
+# gdb rkr
+## (gdb) break wrappers.hh:readlink
 ## (gdb) run --debug --log all --show
+## (gdb) print std::string::max_size()
 ## ```
 ## Call Riker to get the trace
 ## TODO: Normally we would like to plug in Riker and get the actual Trace data structure
 rkr --debug trace -o "$TRACE_FILE"
+
+## Failing test case:
+## CMD_STRING="./test/misc/append_a_line.sh" ./overlay-sandbox/run-sandboxed.sh ./parallel-orch/template_script_to_execute_in_overlay.sh

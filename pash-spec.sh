aab5dbbe09b6585c73f007d72eb7e1c921a75e25
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2023-03-09 13:35:43 -0500

    checkpoint

--- pash-spec.sh
+++ pash-spec.sh
@@ -13,4 +13,8 @@ mkdir -p /tmp/pash_spec
 ## Create a temporary directory where PaSh-Spec can use for temporary files and logs
 export PASH_SPEC_TMP_PREFIX="$(mktemp -d /tmp/pash_spec/pash_XXXXXXX)/"
 
+## Initialize the scheduler-server
+export PASH_SPEC_SCHEDULER_SOCKET="${PASH_SPEC_TMP_PREFIX}/scheduler_socket"
+
+## TODO: Replace this with a call to pa.sh (which will start the scheduler on its own).
 python3 "$PASH_SPEC_TOP/parallel-orch/orch.py" "$@"

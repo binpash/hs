66611b72218d0f0561105c68c910c15b4e8e7349
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2023-03-10 13:01:28 -0500

    add a new testing client

---
+++ scheduler-client.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## TODO: Export PASH_SPEC_TOP
+## TODO: Export PASH_TOP
+
+
+## TODO: Move this file to the other repo
+
+## Only for debugging
+
+export PASH_SPEC_TOP=/home/konstantinos/University/research/pash/dynamic-parallelizer
+## Generate a temporary directory to store the workfiles
+mkdir -p /tmp/pash_spec
+
+## Create a temporary directory where PaSh-Spec can use for temporary files and logs
+export PASH_SPEC_TMP_PREFIX="$(mktemp -d /tmp/pash_spec/pash_XXXXXXX)/"
+
+export PASH_SPEC_SCHEDULER_SOCKET="${PASH_SPEC_TMP_PREFIX}/scheduler_socket"
+
+
+"$PASH_TOP/pa.sh" --speculative "$@"

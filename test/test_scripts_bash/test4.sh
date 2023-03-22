7d8f5d8812fd79b0b248bb184431a68552e052ad
Georgios Liargkovas <gliargovas@aueb.gr>
2023-03-22 12:25:21 -0400

    Add W-W and Backward anti-dependency tests

---
+++ test/test_scripts_bash/test4.sh
@@ -0,0 +1,3 @@
+./sleep_and_cat.sh 2 ./output_bash/in1 ./output_bash/out1
+cat ./output_bash/out1 > ./output_bash/out2
+./sleep_and_cat.sh 1 ./output_bash/in2 ./output_bash/out1
---
+++ test/test_scripts_orch/test4.sh
@@ -0,0 +1,3 @@
+./sleep_and_cat.sh 2 ./output_orch/in1 ./output_orch/out1
+cat ./output_orch/out1 > ./output_orch/out2
+./sleep_and_cat.sh 1 ./output_orch/in2 ./output_orch/out1

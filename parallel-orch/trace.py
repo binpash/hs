6e675afc604e2871ba4f159c5709c03b2d815abd
Georgios Liargkovas <gliargovas@aueb.gr>
2023-03-30 12:31:08 -0400

    Resolve paths for normal files

--- parallel-orch/trace.py
+++ parallel-orch/trace.py
@@ -11,13 +11,15 @@ class Ref(Enum):
     STDOUT = sys.stdout
     STDERR = sys.stderr
     ROOT = os.path.abspath(os.sep)
+    # Maybe Replace instead with this
     CWD = os.getcwd()
+    # CWD = "./"
 
 
 class PathRef:
 
-    def __init__(self, ref_from, path, permissions):
-        self.ref = ref_from
+    def __init__(self, ref, path, permissions):
+        self.ref = ref
         self.path = path
         self.is_read, self.is_write = self.resolve_permissions(permissions)
 
@@ -33,7 +35,10 @@ class PathRef:
         return is_read, is_write
     
     def __str__(self):
-        return f"PathRef({self.ref}, {self.path}, {'r' if self.is_read else '-'}{'w' if self.is_write else '-'}"
+        return f"PathRef({self.ref}, {self.path}, {'r' if self.is_read else '-'}{'w' if self.is_write else '-'})"
+
+    def get_resolved_path(self):
+        return f"{self.ref}{self.path.lstrip('.')}"
 
 # Parse the Riker trace structure
 #
@@ -44,7 +49,7 @@ class PathRef:
 def remove_command_redir(cmd):
     return cmd.split(">")[0].rstrip()
 
-def remove_command_prefix(line):
+def remove_command_prefix(line) -> str:
     return line.split(f"]: ")[1].rstrip()
 
 def get_command_prefix(line):
@@ -99,12 +104,15 @@ def is_no_command_prefix(line):
 def is_launch(line):
     return "Launch(" in line
 
-def get_launch_assignments(trace_item):
+def parse_launch_command(trace_item):
     assert(is_launch(trace_item))
     assignment_suffix = ", ".join(trace_item.split(", ")[1:])
     assignment_string = assignment_suffix[1:-2].split(",")
     assignments = [(x.split("=")) for x in assignment_string]
     return assignments
+
+def parse_launch(trace_item):
+    assert(is_launch(trace_item))
 
 def get_lauch_name(trace_item):
     assert(is_launch(trace_item))
@@ -118,21 +126,17 @@ def get_no_command_ref_id(trace_item):
 def get_no_command_ref_ref(trace_item):
     return trace_item.split("=")[1].strip()
 
+def is_prefix_of_cmd(line, prefix):
+    if prefix is not None and prefix in get_command_prefix(line):
+        return True
+    return False
 
 def parse_rw_sets(trace_object):
-    trace_dict = {}
     refs_dict = {}
+    # In the first iteration, we get the refs
     for line in trace_object:
-        line = line.rstrip()
-        if is_new_path_ref(line):
-            line = remove_command_prefix(line)
-            lhs_ref = get_path_ref_id(line)
-            ref = get_path_ref_ref(line)
-            name = get_path_ref_name(line)
-            open_config = get_path_ref_open_config(line)
-            path_ref = PathRef(ref, name, open_config)
-            trace_dict[lhs_ref] = path_ref
-        elif is_no_command_prefix(line):
+        # This branch will always execute first
+        if is_no_command_prefix(line):
             line = remove_command_prefix(line)
             if "=" in line:
                 lhs_ref = get_no_command_ref_id(line)
@@ -147,18 +151,59 @@ def parse_rw_sets(trace_object):
                     refs_dict[lhs_ref] = Ref.STDIN
                 elif rhs_ref == "STDOUT":
                     refs_dict[lhs_ref] = Ref.STDOUT
-        return refs_dict, trace_dict
+        # Parses launch assignments
+        elif is_launch(line):
+            assignments = parse_launch_command(remove_command_prefix(line))
+            for assignment in assignments:
+                refs_dict[assignment[0].strip()] = refs_dict[assignment[1].strip()]
+        # Parses pathrefs
+        elif is_new_path_ref(line):
+            line = remove_command_prefix(line).strip()
+            lhs_ref = get_path_ref_id(line).strip()
+            ref = get_path_ref_ref(line).strip()
+            name = get_path_ref_name(line).strip()
+            open_config = get_path_ref_open_config(line).strip()
+            path_ref = PathRef(ref, name, open_config)
+            refs_dict[lhs_ref] = path_ref
+    return refs_dict
 
-            
-            
+
+def traverse_path_ref(refs_dict: dict, ref: PathRef):
+    if isinstance(refs_dict[ref.ref], PathRef):
+        return traverse_path_ref(refs_dict, refs_dict[ref.ref])
+    else:
+        return ref.ref
+
+
+def replace_path_ref_terminal_nodes(refs_dict: dict):
+    for ref in refs_dict.values():
+        if isinstance(ref, PathRef):
+            assert (isinstance(refs_dict[ref.ref], Ref))
+            ref.ref = refs_dict[ref.ref].value
+    
+
+def resolve_rw_set_refs(refs_dict):
+    for ref_id, ref in refs_dict.items():
+        if isinstance(ref, PathRef):
+            refs_dict[ref_id].ref = traverse_path_ref(refs_dict, ref)
+    return refs_dict
 
 ## Parse the trace object and gather rw sets for this command
 def parse_and_gather_cmd_rw_sets(trace_object) -> Tuple[set, set]:
-    refs_dict, trace_dict = parse_rw_sets(trace_object)
+    refs_dict = parse_rw_sets(trace_object)
+    resolved_dict = resolve_rw_set_refs(refs_dict)
+    replace_path_ref_terminal_nodes(resolved_dict)
+    
     previous_trace_line = ""
     read_set = set()
     write_set = []
     dir_set = []
+    for k, v in resolved_dict.items():
+        try:
+            logging.debug(f'{k}: {v.get_resolved_path()}')
+        except:
+            logging.debug(f'{k}: {v}')
+    # for trace_item in trace_dict.items()
     for line in trace_object:
         line = line.rstrip()
         if is_command_prefix(line):

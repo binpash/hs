1d5acb94b3ef2e4d04668269e8428862b3419ba5
George Liargkovas <gliargovas@aueb.gr>
2023-04-02 09:45:32 -0400

    Make tests 4 and 5 work anywhere

--- test/template_scripts/test5.sh
+++ test/template_scripts/test5.sh
@@ -1,3 +1,3 @@
-./misc/sleep_and_cat.sh 2 "$test_output_dir/in1" "$test_output_dir/out1"
+"$MISC_SCRIPT_DIR/sleep_and_cat.sh" 2 "$test_output_dir/in1" "$test_output_dir/out1"
 cat "$test_output_dir/out1" > "$test_output_dir/out2"
-./misc/sleep_and_cat.sh 1 "$test_output_dir/in2" "$test_output_dir/in1"
+"$MISC_SCRIPT_DIR/sleep_and_cat.sh" 1 "$test_output_dir/in2" "$test_output_dir/in1"

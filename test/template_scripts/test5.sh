6a52fa460dfb480d6fbf1055b4587ef417f10bf6
Georgios Liargkovas <gliargovas@aueb.gr>
2023-04-04 13:41:28 -0400

    Adjust test running time

--- test/template_scripts/test5.sh
+++ test/template_scripts/test5.sh
@@ -1,5 +1,5 @@
 # Tests that backward dependencies get scheduled correctly
 
-"$MISC_SCRIPT_DIR/sleep_and_cat.sh" 0.6 "$test_output_dir/in1" "$test_output_dir/out1"
+"$MISC_SCRIPT_DIR/sleep_and_cat.sh" 0.2 "$test_output_dir/in1" "$test_output_dir/out1"
 cat "$test_output_dir/out1" > "$test_output_dir/out2"
-"$MISC_SCRIPT_DIR/sleep_and_cat.sh" 0.3 "$test_output_dir/in2" "$test_output_dir/in1"
+"$MISC_SCRIPT_DIR/sleep_and_cat.sh" 0 "$test_output_dir/in2" "$test_output_dir/in1"

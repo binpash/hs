35facec38b96317b56a75329f04aecaba18fcf73
Georgios Liargkovas <gliargovas@aueb.gr>
2023-04-10 13:05:02 -0400

    Add parsing for ExpectResult items

--- parallel-orch/trace.py
+++ parallel-orch/trace.py
@@ -57,6 +57,11 @@ class PathRef:
 
         return os.path.join(commonprefix, ref_without_prefix, path_without_prefix).replace("/./", "/")
 
+class ExpectResult():
+
+    def __init__(self, ref, result):
+        self.ref = ref
+        self.result = result
 
 
 def log_resolved_trace_items(resolved_dict):
@@ -152,8 +157,15 @@ def is_prefix_of_cmd(line, prefix):
         return True
     return False
 
+def is_expect_result(trace_item):
+    return "ExpectResult(" in trace_item
+
+def parse_expect_result(trace_item):
+    return trace_item.lstrip("ExpectResult(").split(")")[0].split(", ")
+
 def parse_rw_sets(trace_object):
     refs_dict = {}
+    expect_result_dict = {}
     # In the first iteration, we get the refs
     for line in trace_object:
         # This branch will always execute first
@@ -172,13 +184,13 @@ def parse_rw_sets(trace_object):
                     refs_dict[lhs_ref] = Ref.STDIN
                 elif rhs_ref == "STDOUT":
                     refs_dict[lhs_ref] = Ref.STDOUT
-        # Parses launch assignments
+        # Parses Launch(...)
         elif is_launch(line):
             assignments = parse_launch_command(remove_command_prefix(line))
             for assignment in assignments:
                 if int(assignment[1].strip().lstrip("r")) in refs_dict:
                     refs_dict[int(assignment[0].strip().lstrip("r"))] = refs_dict[int(assignment[1].strip().lstrip("r"))]
-        # Parses pathrefs
+        # Parses PathRef(...)
         elif is_new_path_ref(line):
             line = remove_command_prefix(line).strip()
             lhs_ref = int(get_path_ref_id(line).strip().lstrip("r"))
@@ -188,7 +200,13 @@ def parse_rw_sets(trace_object):
             no_follow = get_path_ref_no_follow(line)
             path_ref = PathRef(ref, name, open_config, no_follow)
             refs_dict[lhs_ref] = path_ref
-    return refs_dict
+        # Parses ExpectResult(...)
+        elif is_expect_result(line):
+            line = remove_command_prefix(line).strip()
+            ref, result = parse_expect_result(line)
+            ref = int(ref.lstrip("r"))
+            expect_result_dict[ref] = ExpectResult(ref, result)
+    return refs_dict, expect_result_dict
     
 def traverse_path_ref(refs_dict: dict, ref: PathRef):
     if isinstance(ref, PathRef) and not ref.is_nofollow and isinstance(refs_dict[ref.ref], PathRef):
@@ -222,7 +240,7 @@ def replace_path_ref_terminal_nodes(refs_dict: dict):
 
 ## Parse the trace object and gather rw sets for this command
 def parse_and_gather_cmd_rw_sets(trace_object) -> Tuple[set, set]:
-    refs_dict = parse_rw_sets(trace_object)
+    refs_dict, expect_result_dict = parse_rw_sets(trace_object)
     resolved_dict = resolve_rw_set_refs(refs_dict)
     resolved_dict_replaced = replace_path_ref_terminal_nodes(resolved_dict)
     # log_resolved_trace_items(resolved_dict)
@@ -246,9 +264,15 @@ def parse_and_gather_cmd_rw_sets(trace_object) -> Tuple[set, set]:
             if i > 0:
                 if i - 1 in resolved_dict_replaced:
                     previous_resolved_trace_object = resolved_dict_replaced[i-1]
-                    if isinstance(previous_resolved_trace_object, PathRef) and is_path_ref_write(previous_resolved_trace_object):
-                        dir_set.append(resolved_trace_object.get_resolved_path())
-                        write_set.pop()
+                    relevant_current_expect_result = expect_result_dict.get(i-1)
+                    relevant_previous_expect_result = expect_result_dict.get(i)
+                    if relevant_current_expect_result is not None and relevant_previous_expect_result is not None:
+                        if isinstance(previous_resolved_trace_object, PathRef) and \
+                        is_path_ref_write(previous_resolved_trace_object) and \
+                        relevant_current_expect_result.result ==  "SUCCESS" and \
+                        relevant_previous_expect_result.result == "SUCCESS":
+                            dir_set.append(resolved_trace_object.get_resolved_path())
+                            write_set.pop()
 
     prefix = os.path.commonprefix(dir_set)
     suffixes = [dir.replace(prefix, "") for dir in dir_set]

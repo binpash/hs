193f8a2ee9f650ee1126bd483d6bf8673b5ffab7
Georgios Liargkovas <gliargovas@aueb.gr>
2023-04-11 23:23:50 -0400

    Add test script command timing variations

---
+++ test/template_scripts/test2_1.sh
@@ -0,0 +1,13 @@
+# Tests speculation of completely independent cmds
+
+"$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.03 "hello0" "$test_output_dir/out0"
+"$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.06 "hello1" "$test_output_dir/out1"
+"$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.09 "hello2" "$test_output_dir/out2"
+"$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.12 "hello3" "$test_output_dir/out3"
+"$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.15 "hello4" "$test_output_dir/out4"
+"$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.18 "hello5" "$test_output_dir/out5"
+"$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.21 "hello6" "$test_output_dir/out6"
+"$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.24 "hello7" "$test_output_dir/out7"
+"$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.27 "hello8" "$test_output_dir/out8"
+"$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.3  "hello9" "$test_output_dir/out9"
+"$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.33 "hello10" "$test_output_dir/out10"

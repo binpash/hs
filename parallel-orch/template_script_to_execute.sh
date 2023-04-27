54319eea364c485ca2d3c320e8b0ce9510c14ec4
Georgios Liargkovas <gliargovas@aueb.gr>
2023-04-27 16:37:30 -0400

    Add env export to Rikerfile

--- parallel-orch/template_script_to_execute.sh
+++ parallel-orch/template_script_to_execute.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## TODO: Pass frontier flag here instead of separate scripts
+
+
 ## Save the script to execute in the sandboxdir
 echo $CMD_STRING > ./Rikerfile
 ## Save the output shell variables to a file (to pass to the outside context)
@@ -8,12 +11,12 @@ echo $CMD_STRING > ./Rikerfile
 # echo 'bash "$RUNTIME_DIR/pash_declare_vars.sh" "$OUTPUT_VARIABLE_FILE"' >> ./Rikerfile
 # TODO: There is a bug here and parsing of RW dependencies doesn't really work
 #       when we add the following line.
-# echo 'env > "$OUTPUT_VARIABLE_FILE"' >> ./Rikerfile
+echo 'env > "$OUTPUT_VARIABLE_FILE"' >> ./Rikerfile
 # echo 'cat "$OUTPUT_VARIABLE_FILE"' >> ./Rikerfile
 # cat Rikerfile
 ## The (frontier) cmd is run outside a sandbox
 ## so we want to run and trace everything normally
-rkr # --frontier
+rkr --no-inject # --frontier
 ## TODO: Save the exit code here
 rkr --debug trace -o "$TRACE_FILE" > /dev/null
 

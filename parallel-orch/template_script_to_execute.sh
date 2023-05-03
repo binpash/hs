33fffd344918ffe60189babbc8d449074fc9d442
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2023-05-03 15:04:41 -0400

    Change Riker to use bash and correctly save variables in a file (restoring variables hasn't yet happened

--- parallel-orch/template_script_to_execute.sh
+++ parallel-orch/template_script_to_execute.sh
@@ -6,13 +6,14 @@
 ## Save the script to execute in the sandboxdir
 echo $CMD_STRING > ./Rikerfile
 # cat ./Rikerfile 1>&2 # only for debugging
+
 ## Save the output shell variables to a file (to pass to the outside context)
 ## TODO: Currently pash_declare_vars doesn't work because riker invokes /bin/sh
 ##       which is bash in POSIX mode.
 # echo 'bash "$RUNTIME_DIR/pash_declare_vars.sh" "$OUTPUT_VARIABLE_FILE"' >> ./Rikerfile
 # TODO: There is a bug here and parsing of RW dependencies doesn't really work
 #       when we add the following line.
-# echo 'env > "$OUTPUT_VARIABLE_FILE"' >> ./Rikerfile
+echo 'declare -p > "$OUTPUT_VARIABLE_FILE"' >> ./Rikerfile
 # echo 'cat "$OUTPUT_VARIABLE_FILE"' >> ./Rikerfile
 # cat Rikerfile
 ## The (frontier) cmd is run outside a sandbox
@@ -22,5 +23,4 @@ rkr # --frontier
 ## TODO: Save the exit code here
 rkr --debug trace -o "$TRACE_FILE" > /dev/null
 
-## KK 2023-05-03 This probably causes the /dev/tty output (TODO: Check)
 pash_redir_output echo "Sandbox ${CMD_ID} Output variables saved in: $OUTPUT_VARIABLE_FILE"

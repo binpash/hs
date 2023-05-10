03300ab785ae0b6950aa212b71220e889e72dc02
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2023-05-10 10:56:56 -0400

    Add a loop test

---
+++ test/test_scripts/test_loop.sh
@@ -0,0 +1,12 @@
+echo hi
+for i in 1 2 3; do
+    echo hi1
+    sleep 1
+    echo hi2
+done
+
+## Future loop tests must include:
+## 1. A single loop with a single command without anything else 
+## 2. Multiple commands in the same loop
+## 3. Nested loops
+## 4. Commands before and after a loop

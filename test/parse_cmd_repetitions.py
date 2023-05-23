53dbace52c517e221721426a5eb75edb89e264d8
gliargovas <gliargovas@aueb.gr>
2023-05-23 11:43:47 +0300

    Fix repetitions trace parsing for testing

--- test/parse_cmd_repetitions.py
+++ test/parse_cmd_repetitions.py
@@ -6,7 +6,7 @@ import re
 
 with open(sys.argv[1], "r", encoding="UTF8") as f:
     lines = f.read().split("\n")
-REGEX = re.compile(r"TRACE\|.*\|Executions\|")
+REGEX = re.compile(r"TRACE\|.*Executions\|")
 lines = list(filter(REGEX.match, lines))
-lines = [line.split("|")[4] for line in lines]
+lines = [line.split("|")[3].split(",")[1] for line in lines]
 print(" ".join(lines))

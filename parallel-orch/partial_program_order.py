2cfcad2237c600693b9350cf59911f31d3e8fd57
gliargovas <gliargovas@aueb.gr>
2023-06-21 12:52:23 -0600

    Don't move non-zero ec commands to stopped

--- parallel-orch/partial_program_order.py
+++ parallel-orch/partial_program_order.py
@@ -1223,7 +1223,6 @@ class PartialProgramOrder:
         _proc, trace_file, stdout, stderr, variable_file = self.commands_currently_executing.pop(node_id)
         logging.trace(f"ExecutingRemove|{node_id}")
         # Handle stopped by riker due to network access
-        logging.debug("RIKER EXIT CODE: " + str(riker_exit_code))
         if int(riker_exit_code) == 159:
             logging.debug(f" > Adding {node_id} to stopped because it tried to access the network.")
             logging.trace(f"StoppedAdd|{node_id}:network")
@@ -1239,18 +1238,13 @@ class PartialProgramOrder:
             completed_node_info = CompletedNodeInfo(cmd_exit_code, variable_file, stdout)
             self.nodes[node_id].set_completed_info(completed_node_info)
 
-            # Handle any other cmd exit with error
-            # TODO: for now we just postpone them until we reach the frontier
-            #       afterwards we might want to reattempt to speculate them
-            if cmd_exit_code != 0 and node_id not in self.frontier:
-                logging.debug(f" > Adding {node_id} to stopped because it exited with an error.")
-                logging.trace(f"StoppedAdd|{node_id}:error")
-                self.stopped.add(node_id)
-            else:
+            ## We no longer add failed commands to the stopped set, 
+            ## because this leads to more repetitions than needed
+            ## and does not allow us to properly speculate commands
 
-                read_set, write_set = trace.parse_and_gather_cmd_rw_sets(trace_object)
-                rw_set = RWSet(read_set, write_set)
-                self.update_rw_set(node_id, rw_set)
+            read_set, write_set = trace.parse_and_gather_cmd_rw_sets(trace_object)
+            rw_set = RWSet(read_set, write_set)
+            self.update_rw_set(node_id, rw_set)
 
         ## Now that command `node_id` is done executing, we can check which other commands
         ## can be resolved (that might have finished execution before but where waiting on `node_id`)

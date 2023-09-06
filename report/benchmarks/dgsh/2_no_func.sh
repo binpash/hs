af0fcee4d35d298bad9ee283504b1c4851358856
gliargovas <gliargovas@aueb.gr>
2023-09-06 03:22:01 -0600

    Uncomment awk commands

--- report/benchmarks/dgsh/2_no_func.sh
+++ report/benchmarks/dgsh/2_no_func.sh
@@ -8,8 +8,8 @@ git log --format="%an:%ad" --date=default "$@" >"$file1"
 
 echo "Authors ordered by number of commits"
 # Order by frequency
-awk -F: '{print $1}' <"$file1" # | sort | uniq | sort -rn
+awk -F: '{print $1}' <"$file1" | sort | uniq | sort -rn
 
 echo "Days ordered by number of commits"
 # Order by frequency
-# awk -F: '{print substr($2, 1, 3)}' <"$file1"  | sort | uniq | sort -rn
+awk -F: '{print substr($2, 1, 3)}' <"$file1"  | sort | uniq | sort -rn

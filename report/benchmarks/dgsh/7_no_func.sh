eeaa938389bc14910c20743fb40880f372db63d3
gliargovas <gliargovas@aueb.gr>
2023-09-07 06:08:19 -0600

    Comment-out 7.sh awk command

--- report/benchmarks/dgsh/7_no_func.sh
+++ report/benchmarks/dgsh/7_no_func.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Consistent sorting across machines
-export LC_ALL=C
+# export LC_ALL=C
 
 # Temporary files
 file1=$(mktemp)
 file2=$(mktemp)
 file3=$(mktemp)
+file4=$(mktemp)
 
-cat $INPUT_FILE > "$file1"
+cat $INPUT_FILE > $file1
+cat $file1
 
 # Split input one word per line
 tr -cs a-zA-Z '\n' < "$file1" > "$file2"
@@ -35,18 +37,18 @@ awk '{count[$1]++} END {for (i in count) print count[i], i}' < "$file2" |
 sort -rn
 
 # Store number of characters to use in awk below
+
 nchars=$(wc -c < "$file1")
 
 # Character frequency
 # Print absolute
 echo "Character frequency"
-sed 's/./&\
-/g' < "$file1" |
+sed 's/./&\n/g' < "$file1" |
 awk '{count[$1]++} END {for (i in count) print count[i], i}' |
 sort -rn | tee "$file3"
 
 # Print relative
-echo "Relative character frequency"
-awk -v NCHARS=$nchars 'BEGIN {
-		OFMT = "%.2g%%"}
-		{print $1, $2, $1 / NCHARS * 100}' "$file3"
+# echo "Relative character frequency"
+# awk -v NCHARS=$nchars 'BEGIN {
+# 		OFMT = "%.2g%%"}
+# 		{print $1, $2, $1 / NCHARS * 100}' "$file3"

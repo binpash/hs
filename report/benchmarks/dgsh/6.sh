f04d9457084556bf688ac905e5686651fff85f9c
gliargovas <gliargovas@aueb.gr>
2023-09-07 02:35:25 -0600

    Add 7.sh dgsh benchmark without function calls

---
+++ report/benchmarks/dgsh/6.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## Initialize the necessary temporary files
+file1=$(mktemp)
+file2=$(mktemp)
+file3=$(mktemp)
+file4=$(mktemp)
+file5=$(mktemp)
+
+cat $INPUT_FILE > $file1
+
+# Consistent sorting across machines
+export LC_ALL=C
+
+# Stream input from file and split input one word per line
+# Create list of unique words
+tr -cs a-zA-Z '\n' < "$file1" |
+sort -u > "$file2"
+
+# List two-letter palindromes
+sed 's/.*\(.\)\(.\)\2\1.*/p: \1\2-\2\1/;t;g' "$file2" > "$file3"
+
+# List four consecutive consonants
+sed -E 's/.*([^aeiouyAEIOUY]{4}).*/c: \1/;t;g' "$file2" > "$file4"
+
+# List length of words longer than 12 characters
+awk '{if (length($1) > 12) print "l:", length($1);
+	else print ""}' "$file2" > "$file5"
+
+# Paste the four streams side-by-side
+# List only words satisfying one or more properties
+paste "$file2" "$file3" "$file4" "$file5" | 
+fgrep :
---
+++ report/benchmarks/dgsh/7_no_func.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Consistent sorting across machines
+export LC_ALL=C
+
+# Temporary files
+file1=$(mktemp)
+file2=$(mktemp)
+file3=$(mktemp)
+
+cat $INPUT_FILE > "$file1"
+
+# Split input one word per line
+tr -cs a-zA-Z '\n' < "$file1" > "$file2"
+
+# Digram frequency
+echo "Digram frequency"
+perl -ne 'for ($i = 0; $i < length($_) - 2; $i++) {
+	print substr($_, $i, 2), "\n";
+}' < "$file2" |
+awk '{count[$1]++} END {for (i in count) print count[i], i}' |
+sort -rn
+
+# Trigram frequency
+echo "Trigram frequency"
+perl -ne 'for ($i = 0; $i < length($_) - 3; $i++) {
+	print substr($_, $i, 3), "\n";
+}' < "$file2" |
+awk '{count[$1]++} END {for (i in count) print count[i], i}' |
+sort -rn
+
+# Word frequency
+echo "Word frequency"
+awk '{count[$1]++} END {for (i in count) print count[i], i}' < "$file2" |
+sort -rn
+
+# Store number of characters to use in awk below
+nchars=$(wc -c < "$file1")
+
+# Character frequency
+# Print absolute
+echo "Character frequency"
+sed 's/./&\
+/g' < "$file1" |
+awk '{count[$1]++} END {for (i in count) print count[i], i}' |
+sort -rn | tee "$file3"
+
+# Print relative
+echo "Relative character frequency"
+awk -v NCHARS=$nchars 'BEGIN {
+		OFMT = "%.2g%%"}
+		{print $1, $2, $1 / NCHARS * 100}' "$file3"

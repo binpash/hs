a2785008f51a291fa4fc6fc5ba4e4b00fb3ee596
gliargovas <gliargovas@aueb.gr>
2023-11-16 12:25:23 -0700

    Update dgsh benchmark script info

--- report/benchmarks/dgsh/18.sh
+++ report/benchmarks/dgsh/18.sh
@@ -1,16 +1,54 @@
-#!/bin/bash
+#!/bin/sh
 
-# Initialize the necessary temporary files
-file1=$(mktemp)
-file2=$(mktemp)
-file3=$(mktemp)
+# Adapted from: dspinellis/dgsh
+# Source file example/dir.sh
+#!/usr/bin/env pa.sh
+#
+# SYNOPSIS Directory listing
+# DESCRIPTION
+# Windows-like DIR command for the current directory.
+# Modified to use intermediate files instead of command substitution.
+#
+#  Copyright 2012-2013 Diomidis Spinellis
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
 
-# Read the input stream and save to a temporary file
-cat $INPUT_FILE > "$file1"
+# Create temporary files
+free_space_file=$(mktemp)
+file_details_file=$(mktemp)
+file_count_file=$(mktemp)
+dir_count_file=$(mktemp)
+byte_count_file=$(mktemp)
 
-# Process the input in two different ways
-cut -d , -f 5-6 "$file1" > "$file2"
-cut -d , -f 2-4 "$file1" > "$file3"
+# Get free space
+df -h . | awk '!/Use%/{print $4}' > "$free_space_file"
 
-# Merge the processed results
-paste -d , "$file2" "$file3"
+# List details of files and directories
+ls -l | awk '!/^total/ {print $6, $7, $8, $1, sprintf("%8d", $5), $9}' > "$file_details_file"
+
+# Count number of files
+ls -l | grep -v '^total' | grep -v '^d' | wc -l > "$file_count_file"
+
+# Count number of directories
+ls -l | grep '^d' | wc -l > "$dir_count_file"
+
+# Calculate total bytes
+ls -l | awk '{if($1 != "total") s += $5} END {print s}' > "$byte_count_file"
+
+# Display the results
+cat "$file_details_file"
+echo "               $(cat "$file_count_file") File(s) $(cat "$byte_count_file") bytes"
+echo "               $(cat "$dir_count_file") Dir(s) $(cat "$free_space_file") bytes free"
+
+# Clean up temporary files
+rm -f "$free_space_file" "$file_details_file" "$file_count_file" "$dir_count_file" "$byte_count_file"
--- report/benchmarks/dgsh/7.sh
+++ report/benchmarks/dgsh/7.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+# Adapted from: dspinellis/dgsh
+# Source file example/web-log-report.sh
+#
+# SYNOPSIS Web log reporting
+# DESCRIPTION
+# Creates a report for a fixed-size web log file read from the standard input.
+# Demonstrates the combined use of stores and named streams,
+# the use of shell group commands and functions in the scatter block, and
+# the use of cat(1) as a way to sequentially combine multiple streams.
+# Used to measure throughput increase achieved through parallelism.
+#
+#
+# Recommended inputs:
+# https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/3QBYB5
+#
+#  Copyright 2013 Diomidis Spinellis
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+# Consitent sorting across machines
+
 # Consistent sorting
 export LC_ALL=C
 

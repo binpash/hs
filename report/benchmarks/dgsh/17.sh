a98b95603e6042dfe6b59f735d59aa27f8558991
gliargovas <gliargovas@aueb.gr>
2024-02-11 12:40:48 -0700

    Fix benchmarks to work properly

--- report/benchmarks/dgsh/17.sh
+++ report/benchmarks/dgsh/17.sh
@@ -38,20 +38,13 @@ file2=$(mktemp)
 file3=$(mktemp)
 file4=$(mktemp)
 
-# Save the ls output to a temporary file
-ls -n > "$file1"
+cat $INPUT_FILE > $file1
 
-# Reorder fields in DIR-like way
-awk '!/^total/ {print $6, $7, $8, $1, sprintf("%8d", $5), $9}' "$file1" > "$file2"
+# Extract columns 5 and 6, save to temp1
+cut -d ',' -f 5-6 "$file1" > "$file2"
 
-# Count number of files
-wc -l "$file1" | tr -d \\n > "$file3"
-echo -n ' File(s) ' >> "$file3"
-awk '{s += $5} END {printf("%d bytes\n", s)}' "$file1" >> "$file3"
+# Extract columns 2, 3, and 4, save to temp2
+cut -d ',' -f 2-4 "$file1" > "$file3"
 
-# Count number of directories and print label for number of dirs and calculate free bytes
-grep -c '^d' "$file1" | tr -d \\n > "$file4"
-df -h . | awk '!/Use%/{print " Dir(s) " $4 " bytes free"}' >> "$file4"
-
-# Display the results
-cat "$file2" "$file3" "$file4"
+# Combine the columns
+paste -d ',' "$file2" "$file3"

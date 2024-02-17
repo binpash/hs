1172d7fbb12f29c7348a78c0319e895a3b1b4a97
gliargovas <gliargovas@aueb.gr>
2024-02-17 16:08:08 +0000

    Remove Riker dependency

--- scripts/install_deps_ubuntu20.sh
+++ scripts/install_deps_ubuntu20.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-## Install Riker's dependencies
 sudo apt-get update
-sudo apt install -y make clang llvm git gcc python3-cram file graphviz libtool
-sudo update-alternatives --install /usr/bin/cram cram /usr/bin/cram3 100
+# TODO: some of these are Riker dependencies are no longer needed.
+sudo apt install -y make clang llvm git gcc python3-cram file graphviz libtool python3-matplotlib
 
 export PASH_SPEC_TOP=${PASH_SPEC_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}
 export PASH_TOP=${PASH_TOP:-$PASH_SPEC_TOP/deps/pash}
 
-pip3 install $PASH_SPEC_TOP/requirements.txt
-
 ## Download submodule dependencies
 git submodule update --init --recursive
-
-## Install Riker
-(cd deps/riker; make; sudo make install)
 
 ## Install PaSh
 (cd deps/pash; ./scripts/distro-deps.sh; ./scripts/setup-pash.sh)

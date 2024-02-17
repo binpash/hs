d0851fdd5988b6584e96d3c2464af29ff508cbc6
gliargovas <gliargovas@aueb.gr>
2024-02-17 16:11:36 +0000

    Update config to also setup try

--- scripts/install_deps_ubuntu20.sh
+++ scripts/install_deps_ubuntu20.sh
@@ -2,13 +2,16 @@
 
 sudo apt-get update
 # TODO: some of these are Riker dependencies are no longer needed.
-sudo apt install -y make clang llvm git gcc python3-cram file graphviz libtool python3-matplotlib
+sudo apt install -y make git python3-cram file graphviz libtool python3-matplotlib libcap2-bin mergerfs
 
 export PASH_SPEC_TOP=${PASH_SPEC_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}
 export PASH_TOP=${PASH_TOP:-$PASH_SPEC_TOP/deps/pash}
 
 ## Download submodule dependencies
 git submodule update --init --recursive
+
+# Install try
+(cd deps/try; ./setup.sh)
 
 ## Install PaSh
 (cd deps/pash; ./scripts/distro-deps.sh; ./scripts/setup-pash.sh)

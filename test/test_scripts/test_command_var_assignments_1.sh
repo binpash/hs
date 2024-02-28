78b970eb33f0e39d046fd61e9a8691c8e8e1f59c
gliargovas <gliargovas@aueb.gr>
2024-02-28 14:18:18 -0700

    Make export test cases work properly

--- test/test_scripts/test_command_var_assignments_1.sh
+++ test/test_scripts/test_command_var_assignments_1.sh
@@ -1,8 +1,8 @@
-"$MISC_SCRIPT_DIR/sleep_and_grep.sh" 0.4 
-"$MISC_SCRIPT_DIR/sleep_and_export.sh" 0.35 "foo" "hello"
-"$MISC_SCRIPT_DIR/sleep_and_export.sh" 0.3 "bar" "world"
-"$MISC_SCRIPT_DIR/sleep_and_export.sh" 0.25 "filename" "out1.txt"
+export foo=$("$MISC_SCRIPT_DIR/sleep_and_return.sh" 0.35 "hello")
+export bar=$("$MISC_SCRIPT_DIR/sleep_and_return.sh" 0.3 "world")
+export filename=$("$MISC_SCRIPT_DIR/sleep_and_return.sh" 0.25 "out1")
 "$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.2 "$foo" "$test_output_dir/$filename"
-"$MISC_SCRIPT_DIR/sleep_and_export.sh" 0.15 "foo" "hello"
+export foo=$("$MISC_SCRIPT_DIR/sleep_and_return.sh" 0.15 "HELLO")
 "$MISC_SCRIPT_DIR/sleep_and_cat.sh" 0.1 "$test_output_dir/$filename" "$test_output_dir/out2"
 "$MISC_SCRIPT_DIR/sleep_and_echo.sh" 0.05 "$foo $bar" "$test_output_dir/out2"
+cat "$test_output_dir/out1" "$test_output_dir/out2"

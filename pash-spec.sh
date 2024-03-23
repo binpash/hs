ca08c8b7264990bdd4a614f487abea58f76b9d05
Guest <di_jin@brown.edu>
2024-03-23 18:11:34 -0400

    pdbhack

--- pash-spec.sh
+++ pash-spec.sh
@@ -14,12 +14,16 @@ mkdir -p /tmp/pash_spec
 ## Delete any Riker cache
 rm -rf ./.rkr
 
+export PASH_SPEC_TMP_PREFIX=/tmp/pash_spec/pash_test
+mkdir -p $PASH_SPEC_TMP_PREFIX
 ## Create a temporary directory where PaSh-Spec can use for temporary files and logs
-export PASH_SPEC_TMP_PREFIX="$(mktemp -d /tmp/pash_spec/pash_XXXXXXX)/"
+# export PASH_SPEC_TMP_PREFIX="$(mktemp -d /tmp/pash_spec/pash_XXXXXXX)/"
 
 ## Initialize the scheduler-server
 export PASH_SPEC_SCHEDULER_SOCKET="${PASH_SPEC_TMP_PREFIX}/scheduler_socket"
 
 ## TODO: Replace this with a call to pa.sh (which will start the scheduler on its own).
-# python3 "$PASH_SPEC_TOP/parallel-orch/orch.py" "$@"
-"$PASH_TOP/pa.sh" --speculative "$@"
+a=$1
+shift
+"$PASH_TOP/pa.sh" $a --speculative "$@"
+

086eb4b2e89d3db8ea3f3505463156750060b03d
Guest <di_jin@brown.edu>
2024-03-30 23:32:24 -0400

    update chmod for cgroup

--- pash-spec.sh
+++ pash-spec.sh
@@ -11,6 +11,8 @@ export PASH_TOP=${PASH_TOP:-$PASH_SPEC_TOP/deps/pash}
 sudo mkdir -p /sys/fs/cgroup/frontier
 total_mem=$(free | awk '/Mem:/ { print $2 }')
 protected_mem=$(python3 -c "print(int(${total_mem}*0.75) << 10)")
+sudo chmod 666 /sys/fs/cgroup/cgroup.procs
+sudo chmod 666 /sys/fs/cgroup/frontier/cgroup.procs
 sudo bash -c "echo $protected_mem > /sys/fs/cgroup/frontier/memory.min"
 
 ## Generate a temporary directory to store the workfiles

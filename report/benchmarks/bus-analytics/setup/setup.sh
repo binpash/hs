10e7cf5d5b7cfa39d9e84227e4b63a00c0c71fcb
gliargovas <gliargovas@aueb.gr>
2024-04-02 17:29:07 -0400

    Bus analytics benchmark final config

--- report/benchmarks/bus-analytics/setup/setup.sh
+++ report/benchmarks/bus-analytics/setup/setup.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+export PATH=$PATH:$HOME/.local/bin
+export PASH_SPEC_TOP=${PASH_SPEC_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}
+export PASH_TOP=${PASH_TOP:-$PASH_SPEC_TOP/deps/pash}
+
+resource_dir="$PASH_SPEC_TOP/report/resources"
+download_dir="$resource_dir/bus-analytics"
+
+cd "$download_dir"
+
 if [ ! -f bus.csv ]; then
     echo "Downloading full-size dataset..."
     wget -nc -O bus.csv.bz2 'https://www.balab.aueb.gr/~dds/oasa-2021-01-08.bz2'

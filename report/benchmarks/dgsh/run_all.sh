3f4783c5e9a0cd3bb04098a4b32dda2fc0733bc6
gliargovas <gliargovas@aueb.gr>
2024-04-15 18:01:14 -0500

    Add dgsh multi-window runner

---
+++ report/benchmarks/dgsh/run_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export PATH=$PATH:$HOME/.local/bin
+export PASH_SPEC_TOP=${PASH_SPEC_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}
+export PASH_TOP=${PASH_TOP:-$PASH_SPEC_TOP/deps/pash}
+
+output_dir="$PASH_SPEC_TOP/report/output/dgsh" # Adjust the path as necessary
+download_dir="$PASH_SPEC_TOP/report/resources/dgsh"
+
+./run --target sh-only
+mkdir -p $output_dir/base
+rm $download_dir/*.txt
+mv $output_dir/* $output_dir/base
+
+for window in 0 1 2 4 8 16 32 64
+do
+    ./run --target hs-only --window $window
+    rm $download_dir/*.txt
+    mkdir -p $output_dir/$window
+    mv $output_dir/* $output_dir/$window
+done
+

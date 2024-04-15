a90ef4766220b1ebe5d354389e5b502789c7807e
gliargovas <gliargovas@aueb.gr>
2024-04-15 18:15:56 -0500

    Fix multi-benchmark runner

--- report/benchmarks/unix_50/run_all.sh
+++ report/benchmarks/unix_50/run_all.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
 export PATH=$PATH:$HOME/.local/bin
 export PASH_SPEC_TOP=${PASH_SPEC_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}
 export PASH_TOP=${PASH_TOP:-$PASH_SPEC_TOP/deps/pash}
 
-output_dir="$PASH_SPEC_TOP/report/output/dgsh" # Adjust the path as necessary
-download_dir="$PASH_SPEC_TOP/report/resources/dgsh"
-result_dir="$PASH_SPEC_TOP/results/dgsh"
+output_dir="$PASH_SPEC_TOP/report/output/unix_50"
+download_dir="$PASH_SPEC_TOP/report/resources/unix_50"
+result_dir="$PASH_SPEC_TOP/results/unix_50"
 
 ./run --target sh-only
 mkdir -p $result_dir/base
@@ -15,7 +13,6 @@ mv $output_dir/* $result_dir/base
 for window in 0 1 2 4 8 16 32 64
 do
     ./run --target hs-only --window $window
-    mkdir -p $output_dir/$result_dir
-    mv $output_dir/* $output_dir/$result_dir
+    mkdir -p $result_dir/$window
+    mv $output_dir/* $result_dir/$window
 done
-

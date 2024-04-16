dd54c8e021a044c2d21d6a6652d897abbf933b6a
gliargovas <gliargovas@aueb.gr>
2024-04-15 22:17:20 -0500

    Add second validation run on max_temp

--- report/benchmarks/max_temp/run_all.sh
+++ report/benchmarks/max_temp/run_all.sh
@@ -6,18 +6,29 @@ export PASH_TOP=${PASH_TOP:-$PASH_SPEC_TOP/deps/pash}
 
 output_dir="$PASH_SPEC_TOP/report/output/max_temp" # Adjust the path as necessary
 download_dir="$PASH_SPEC_TOP/report/resources/max_temp"
-result_dir="$PASH_SPEC_TOP/results/dgsh"
+result_dir="${result_dir:$PASH_SPEC_TOP/results/dgsh}"
+
+rm -f $download_dir/*.txt
 
 ./run --target sh-only
 rm $download_dir/*.txt
-mkdir -p $result_dir/base
-mv $output_dir/* $result_dir/base
+mkdir -p $result_dir/base/a
+mv $output_dir/* $result_dir/base/a
 
-for window in 0 1 2 4 8 16 32 64 128
+./run --target sh-only
+rm $download_dir/*.txt
+mkdir -p $result_dir/base/b
+mv $output_dir/* $result_dir/base/b
+
+for window in 0 8 16 32 64 128
 do
     ./run --target hs-only --window $window
     rm $download_dir/*.txt
-    mkdir -p $result_dir/$window
+    mkdir -p $result_dir/$window/a
+    mv $output_dir/* $result_dir/$window
+    ./run --target hs-only --window $window
+    rm $download_dir/*.txt
+    mkdir -p $result_dir/$window/b
     mv $output_dir/* $result_dir/$window
 done
 

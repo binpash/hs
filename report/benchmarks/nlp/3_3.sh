0f21eec959e828423a655cf31ba0b3e1f98d26b0
danliu <nad2040@gmail.com>
2024-05-22 15:11:15 +0000

    nlp

---
+++ report/benchmarks/nlp/3_3.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# tag: sort_words_by_rhyming.sh
+# set -e
+
+IN=${IN:-$PWD/pg}
+OUT=${OUT:-$PWD/output/3_3/}
+ENTRIES=${ENTRIES:-10}
+mkdir -p "$OUT"
+
+for input in $(ls ${IN} | head -n ${ENTRIES})
+do
+    cat $IN/$input | tr -sc '[A-Z][a-z]' '[\012*]' | sort | uniq -c | rev | sort | rev > ${OUT}/${input}.out
+done
+
+echo 'done';
---
+++ report/benchmarks/nlp/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# rm -rf genesis exodus pg
+rm -rf output

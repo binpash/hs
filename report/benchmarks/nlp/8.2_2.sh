1a10580b93383b598443baf2b0bc15d064ddac6a
danliu <nad2040@gmail.com>
2024-05-22 15:15:10 +0000

    nlp: expand function in loops. others can't be easily unrolled

--- report/benchmarks/nlp/8.2_2.sh
+++ report/benchmarks/nlp/8.2_2.sh
@@ -8,18 +8,17 @@ OUT=${OUT:-$PWD/output/8_2_2/}
 ENTRIES=${ENTRIES:-10}
 mkdir -p "$OUT"
 
-run_tests() {
-    input=$1
-    cat $IN/$input | tr -sc '[A-Z][a-z]' '[\012*]' > ${OUT}/${input}.input.words
-    tail +2 ${OUT}/${input}.input.words > ${OUT}/${input}.input.nextwords
-    paste ${OUT}/${input}.input.words ${OUT}/${input}.input.nextwords | sort | uniq -c > ${OUT}/${input}.input.bigrams
-    awk "\$1 == 2 {print \$2, \$3}" ${OUT}/${input}.input.bigrams
-}
+#run_tests() {
+#    input=$1
+#}
 
 #export -f run_tests
 for input in $(ls ${IN} | head -n ${ENTRIES})
 do
-    run_tests $input  > ${OUT}/${input}.out
+    cat $IN/$input | tr -sc '[A-Z][a-z]' '[\012*]' > ${OUT}/${input}.input.words
+    tail +2 ${OUT}/${input}.input.words > ${OUT}/${input}.input.nextwords
+    paste ${OUT}/${input}.input.words ${OUT}/${input}.input.nextwords | sort | uniq -c > ${OUT}/${input}.input.bigrams
+    awk "\$1 == 2 {print \$2, \$3}" ${OUT}/${input}.input.bigrams > ${OUT}/${input}.out
 done
 
 echo 'done';

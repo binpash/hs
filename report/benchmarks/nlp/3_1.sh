f84d62cb9fe5bf6b00ba41b2f83e7851ef422bb2
danliu <nad2040@gmail.com>
2024-05-22 15:11:15 +0000

    nlp

---
+++ report/benchmarks/nlp/3_1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# tag: sort
+# set -e
+
+IN=${IN:-$PWD/pg}
+OUT=${OUT:-$PWD/output/3_1/}
+ENTRIES=${ENTRIES:-10}
+mkdir -p "$OUT"
+
+for input in $(ls ${IN} | head -n ${ENTRIES})
+do
+    cat $IN/$input | tr -sc '[A-Z][a-z]' '[\012*]' | sort | uniq -c | sort -nr > ${OUT}/${input}.out
+done
+
+echo 'done';

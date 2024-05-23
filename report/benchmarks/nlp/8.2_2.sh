b5bcb364f01cf24b896ba82ab8e04a440885f75a
danliu <nad2040@gmail.com>
2024-05-23 17:27:41 +0000

    unrolled scripts are separate

--- report/benchmarks/nlp/8.2_2.sh
+++ report/benchmarks/nlp/8.2_2.sh
@@ -8,17 +8,18 @@ OUT=${OUT:-$PWD/output/8_2_2/}
 ENTRIES=${ENTRIES:-10}
 mkdir -p "$OUT"
 
-#run_tests() {
-#    input=$1
-#}
+run_tests() {
+    input=$1
+    cat $IN/$input | tr -sc '[A-Z][a-z]' '[\012*]' > ${OUT}/${input}.input.words
+    tail +2 ${OUT}/${input}.input.words > ${OUT}/${input}.input.nextwords
+    paste ${OUT}/${input}.input.words ${OUT}/${input}.input.nextwords | sort | uniq -c > ${OUT}/${input}.input.bigrams
+    awk "\$1 == 2 {print \$2, \$3}" ${OUT}/${input}.input.bigrams
+}
 
 #export -f run_tests
 for input in $(ls ${IN} | head -n ${ENTRIES})
 do
-    cat $IN/$input | tr -sc '[A-Z][a-z]' '[\012*]' > ${OUT}/${input}.input.words
-    tail +2 ${OUT}/${input}.input.words > ${OUT}/${input}.input.nextwords
-    paste ${OUT}/${input}.input.words ${OUT}/${input}.input.nextwords | sort | uniq -c > ${OUT}/${input}.input.bigrams
-    awk "\$1 == 2 {print \$2, \$3}" ${OUT}/${input}.input.bigrams > ${OUT}/${input}.out
+    run_tests $input  > ${OUT}/${input}.out
 done
 
 echo 'done';

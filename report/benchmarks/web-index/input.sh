ee55844af4450a952178c6798d8308b06fd9fe30
gliargovas <gliargovas@aueb.gr>
2024-06-06 07:32:36 +0000

    Fix log analysis

--- report/benchmarks/web-index/input.sh
+++ report/benchmarks/web-index/input.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 
-wiki_archive="https://dumps.wikimedia.org/other/static_html_dumps/current/en/wikipedia-en-html.tar.7z"
 BENCH_TOP=${BENCH_TOP:-$(git rev-parse --show-toplevel)}
 RESOURCES_DIR=${RESOURCES_DIR:-$BENCH_TOP/report/resources/web-index/}
 
@@ -7,21 +7,15 @@ mkdir -p $RESOURCES_DIR
 
 if [[ ! -d "$RESOURCES_DIR/en" ]]; then
   if [ "$1" = "--small" ]; then
-    # 500 entries
-    wget -0 $RESOURCES_DIR/wikipedia500.tar.gz https://atlas-group.cs.brown.edu/data/web-index/wikipedia500.tar.gz 
-    wget -0 $RESOURCES_DIR/index500.txt https://atlas-group.cs.brown.edu/data/web-index/index500.txt 
-    tar -xf $RESOURCES_DIR/wikipedia500.tar.gz -C $RESOURCES_DIR
-  elif [ "$1" = "--meduim" ]; then
-    the default full
     # 1000 entries
-    wget https://atlas-group.cs.brown.edu/data/web-index/wikipedia1000.tar.gz -0 $RESOURCES_DIR/wikipedia1000.tar.gz
-    wget https://atlas-group.cs.brown.edu/data/web-index/index1000.txt -0 $RESOURCES_DIR/index1000.txt
-    tar -xf $RESOURCES_DIR/wikipedia1000.tar.gz -C $RESOURCES_DIR
+    wget -O $RESOURCES_DIR/wikipedia-small.tar.gz https://atlas-group.cs.brown.edu/data/wikipedia/input_small/articles.tar.gz
+    wget -O $RESOURCES_DIR/index_small.txt https://atlas-group.cs.brown.edu/data/wikipedia/input_small/index.txt 
+    tar -xf $RESOURCES_DIR/wikipedia-small.tar.gz -C $RESOURCES_DIR
   else
     # full dataset
     echo "Downloading the full dataset. Caution!! file around 210GB"
-    wget -O wikipedia-en-html.tar.7z $wiki_archive
-    7z x wikipedia-en-html.tar.7z
-    tar -xvf wikipedia-en-html.tar -C $RESOURCES_DIR
+    wget -O $RESOURCES_DIR/wikipedia.tar.gz https://atlas-group.cs.brown.edu/data/wikipedia/input/articles.tar.gz
+    wget -O $RESOURCES_DIR/index.txt https://atlas-group.cs.brown.edu/data/wikipedia/input/index.txt
+    tar -xf $RESOURCES_DIR/wikipedia.tar.gz -C $RESOURCES_DIR
   fi
 fi

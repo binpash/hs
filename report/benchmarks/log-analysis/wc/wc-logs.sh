f1666eb579ecd44d41d4bdb01eb462e072f96400
danliu <nad2040@gmail.com>
2024-07-25 18:59:34 +0000

    log-analysis: generated files are the same. stdout still different

--- report/benchmarks/log-analysis/wc/wc-logs.sh
+++ report/benchmarks/log-analysis/wc/wc-logs.sh
@@ -20,27 +20,27 @@ for gz_file in ${INPUT_DIR}/*.gz; do
 
     # Find broken links (404)
     echo "Finding broken links (404)..."
-    awk '($9 ~ /404/)' "$output_file" | awk '{print $7}' | sort | uniq -c | sort -rn > "${OUTPUT_DIR}/${INPUT}_404_broken_links.log"
+    awk "(\$9 ~ /404/)" "$output_file" | awk "{print \$7}" | sort | uniq -c | sort -rn > "${OUTPUT_DIR}/${INPUT}_404_broken_links.log"
 
     # For 502 (bad-gateway)
     echo "Finding 502 errors (bad-gateway)..."
-    awk '($9 ~ /502/)' "$output_file" | awk '{print $7}' | sort | uniq -c | sort -r > "${OUTPUT_DIR}/${INPUT}_502_errors.log"
+    awk "(\$9 ~ /502/)" "$output_file" | awk "{print \$7}" | sort | uniq -c | sort -r > "${OUTPUT_DIR}/${INPUT}_502_errors.log"
 
     # Who are requesting broken links (or URLs resulting in 502)
     echo "Finding requests for wp-admin/install.php..."
-    awk -F\" '($2 ~ "/wp-admin/install.php"){print $1}' "$output_file" | awk '{print $1}' | sort | uniq -c | sort -r > "${OUTPUT_DIR}/${INPUT}_requests_wp_admin.log"
+    awk -F\" "(\$2 ~ \"/wp-admin/install.php\"){print \$1}" "$output_file" | awk "{print \$1}" | sort | uniq -c | sort -r > "${OUTPUT_DIR}/${INPUT}_requests_wp_admin.log"
 
     # 404 for php files - mostly hacking attempts
     echo "Finding 404 errors for PHP files..."
-    awk '($9 ~ /404/)' "$output_file" | awk -F\" '($2 ~ "^GET .*\.php")' | awk '{print $7}' | sort | uniq -c | sort -r | head -n 20 > "${OUTPUT_DIR}/${INPUT}_404_php_files.log"
+    awk "(\$9 ~ /404/)" "$output_file" | awk -F\" "(\$2 ~ \"^GET .*\.php\")" | awk "{print \$7}" | sort | uniq -c | sort -r | head -n 20 > "${OUTPUT_DIR}/${INPUT}_404_php_files.log"
 
     # Most requested URLs
     echo "Finding most requested URLs..."
-    awk -F\" '{print $2}' "$output_file" | awk '{print $2}' | sort | uniq -c | sort -r > "${OUTPUT_DIR}/${INPUT}_most_requested_urls.log"
+    awk -F\" "{print \$2}" "$output_file" | awk "{print \$2}" | sort | uniq -c | sort -r > "${OUTPUT_DIR}/${INPUT}_most_requested_urls.log"
 
     # Most requested URLs containing "ref"
     echo "Finding most requested URLs containing 'ref'..."
-    awk -F\" '($2 ~ "ref"){print $2}' "$output_file" | awk '{print $2}' | sort | uniq -c | sort -r > "${OUTPUT_DIR}/${INPUT}_most_requested_urls_ref.log"
+    awk -F\" "(\$2 ~ \"ref\"){print \$2}" "$output_file" | awk "{print \$2}" | sort | uniq -c | sort -r > "${OUTPUT_DIR}/${INPUT}_most_requested_urls_ref.log"
 
     echo "Processing of $output_file completed."
 done

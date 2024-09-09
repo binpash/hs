d022172e760c8e269fc9e21372f3d84143d12231
danliu <nad2040@gmail.com>
2024-09-09 03:34:49 +0000

    fix teraseq

--- report/benchmarks/teraseq/Dockerfile.hs
+++ report/benchmarks/teraseq/Dockerfile.hs
@@ -2,23 +2,32 @@ FROM teraseq20-data
 RUN mkdir -p /srv/hs
 WORKDIR /srv/hs
 SHELL ["/bin/bash", "-c"]
-ARG DEBIAN_FRONTEND=noninteractive
+# ARG DEBIAN_FRONTEND=noninteractive
 RUN ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 RUN apt update
 RUN apt install -y sudo curl git
-ENV PASH_SPEC_TOP=/srv/hs
-ENV PASH_TOP=/srv/hs/deps/pash
 RUN conda install -y -c anaconda python=3.11
-RUN apt install -y vim strace make python3-cram file graphviz libtool python3-matplotlib libcap2-bin mergerfs lsb-release
+RUN apt install -y vim strace make python3-cram file graphviz libtool python3-matplotlib libcap2-bin mergerfs lsb-release sudo git python3 wget util-linux
+# pash distro deps
+RUN apt install -y bc curl graphviz bsdmainutils libffi-dev locales locales-all netcat-openbsd pkg-config procps python3-pip python3-setuptools python3-testresources wamerican-insane
+# try deps
+RUN apt install -y expect mergerfs attr
 COPY . .
 RUN /srv/hs/report/benchmarks/teraseq/setup
+
+RUN git config --global --add safe.directory /srv
 RUN git submodule update --init --recursive
-WORKDIR /srv/hs/deps/pash
-RUN scripts/distro-deps.sh
-RUN scripts/setup-pash.sh
+ENV PASH_SPEC_TOP=/srv/hs
+ENV PASH_TOP=/srv/hs/deps/pash
 WORKDIR /srv/hs/deps/try
-RUN ./setup.sh
+RUN make -C utils
+RUN mv utils/try-commit /bin
+RUN mv utils/try-summary /bin
+WORKDIR /srv/hs/deps/pash
+RUN ./scripts/setup-pash.sh
+
 WORKDIR /srv/hs
 RUN chmod +x entrypoint.sh
-WORKDIR /root/TERA-Seq_manuscript/samples
+
 RUN mv /srv/hs/report/benchmarks/teraseq/annotate-sqlite-with-fastq.R /root/TERA-Seq_manuscript/tools/utils/
+WORKDIR /root/TERA-Seq_manuscript/samples

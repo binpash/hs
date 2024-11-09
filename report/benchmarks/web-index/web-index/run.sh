aa6c88497f4f97d70072cfdcc58a47721a555620
gliargovas <gliargovas@aueb.gr>
2024-11-09 15:34:04 -0700

    Fix errors in webindex and add output file comparison

--- report/benchmarks/web-index/web-index/run.sh
+++ report/benchmarks/web-index/web-index/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkfifo {1,2,3}grams
 
+$OUTPUT_DIR="$OUTPUT_DIR"
 extract_text="$SCRIPT_DIR/extract_text.sh"
 bigrams_aux="$SCRIPT_DIR/bigrams_aux.sh"
 trigrams_aux="$SCRIPT_DIR/trigrams_aux.sh"
@@ -12,27 +12,25 @@ cat $INPUT_FILE |
   tr A-Z a-z |
   grep -vwFf $WEB_INDEX_DIR/stopwords.txt |
   $WEB_INDEX_DIR/stem-words.js |
-  tee 3grams 2grams 1grams
+  tee "$OUTPUT_DIR/3grams" "$OUTPUT_DIR/2grams" "$OUTPUT_DIR/1grams"
 
-cat 1grams |
+cat "$OUTPUT_DIR/1grams" |
     sort |
     uniq -c |
-    sort -rn > 1-grams.txt
+    sort -rn > "$OUTPUT_DIR/1-grams.txt"
 
-cat 2grams |
+cat "$OUTPUT_DIR/2grams" |
     tr -cs A-Za-z '\n' |
     tr A-Z a-z |
     $bigrams_aux |
     sort |
     uniq -c |
-    sort -rn > 2-grams.txt
+    sort -rn > "$OUTPUT_DIR/2-grams.txt"
 
-cat 3grams |
+cat "$OUTPUT_DIR/3grams" |
     tr -cs A-Za-z '\n' |
     tr A-Z a-z |
     $trigrams_aux |
     sort |
     uniq -c |
-    sort -rn > 3-grams.txt
-
-rm -f {1,2,3}grams
+    sort -rn > "$OUTPUT_DIR/3-grams.txt"

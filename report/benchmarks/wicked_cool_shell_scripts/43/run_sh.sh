14fa367dd78645099fa1c32de769753dfdc6ce52
gliargovas <gliargovas@aueb.gr>
2024-11-19 01:35:59 -0500

    Create button spec for sysadmin 43 and 48

--- report/benchmarks/wicked_cool_shell_scripts/43/run_sh.sh
+++
@@ -1,2 +0,0 @@
-export SHELL=/bin/bash
-./43.sh > result_sh_out
--- report/benchmarks/wicked_cool_shell_scripts/43/verify.sh
+++
@@ -1,2 +0,0 @@
-grep -q "Your environment checks out fine." result_sh_out || echo "sh fail" > result_error
-grep -q "Your environment checks out fine." result_hs_out || echo "hs fail" >> result_error

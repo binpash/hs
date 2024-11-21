e7137943a70c613bf91dd65948854d21ead4693a
gliargovas <gliargovas>
2024-11-21 12:50:32 -0500

    Fix 52

--- report/benchmarks/wicked_cool_shell_scripts/52/52.sh
+++ report/benchmarks/wicked_cool_shell_scripts/52/52.sh
@@ -25,14 +25,14 @@ fi
 dirsize="$(du -s $1 | awk '{print $1}')"
 if [ $dirsize -gt $maxarchivedir ] ; then
   /bin/echo -n "Warning: directory $1 is $dirsize blocks. Proceed? [n] " 
-  read answer
+  # read answer
   answer="$(echo $answer | tr '[:upper:]' '[:lower:]' | cut -c1)"
   if [ "$answer" != "y" ] ; then
     echo "${progname}: archive of directory $1 canceled." >&2
     exit 0
   fi
 fi
-archivename="$1.tgz"
+archivename="$RESOURCE_DIR/$1.tgz"
 if tar cf - $1 | $compress > $archivename ; then
   echo "Directory $1 archived as $archivename"
 else

a0503092313821ec4b6ace5992030c4630c345ec
Di Jin <di_jin@brown.edu>
2025-05-09 17:44:28 +0000

    only run hs

--- report/run.sh
+++ report/run.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 BASE=$(dirname $(realpath "$0"))
 HSTMP=/tmp/hs_tmp
 
 for b in $(cat $BASE/koala_benchmarks); do bs=$(echo $b | cut -d '/' -f 1); $BASE/benchmarks/$bs/setup; done
 
 mkdir -p $HSTMP
-for b in $(cat $BASE/koala_benchmarks); do $BASE/benchmarks/$b/run $HSTMP; done
+for b in $(cat $BASE/koala_benchmarks); do $BASE/benchmarks/$b/run $HSTMP --target hs-only; done
 sudo rm -rf $HSTMP

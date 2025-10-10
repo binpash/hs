7711c58a968f0b8efb623e545ac72241ebf1439f
Bolun Thompson <bolunthompson@ucla.edu>
2025-10-10 05:31:38 -0700

    fix: environment

--- python_hs/entrypoint.sh
+++ python_hs/entrypoint.sh
@@ -2,7 +2,7 @@
 
 source "$PASH_TOP/compiler/orchestrator_runtime/pash_init_setup.sh" --speculative
 # point to the local downloaded folders
-export PYTHONPATH="$PASH_TOP/python_pkgs/:$PASH_SPEC_TOP/python_hs/python_hs/:$PYTHONPATH"
+export PYTHONPATH="$PASH_TOP/python_pkgs/:$PASH_SPEC_TOP/python_hs/:$PYTHONPATH"
 
 # sets daemon_pid
 start_server

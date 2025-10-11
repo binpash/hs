a912ac67ac378f5cc19e4bf7b6848381ca2393c4
Bolun Thompson <bolunthompson@ucla.edu>
2025-10-11 07:46:16 -0700

    add bioinfo download data script

---
+++ python_hs/benchmarks/bioinfo-automine/download_data.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Setup script with inlined data file for benchmark
+
+#Author: Hamid D. Ismail, Ph.D.
+#Book title: Bioinformatics of Autoimmune Diseases
+
+sra_ids=(
+    SRR26147696
+    SRR26147697
+    SRR26147702
+    SRR26147714
+    SRR26147715
+    SRR26147716
+)
+
+# Check if fasterq-dump is available
+if ! command -v fasterq-dump &> /dev/null; then
+    echo "Error: fasterq-dump is not installed or not in PATH."
+    exit 1
+fi
+
+# Set number of threads
+threads=8
+# Download paired-end FASTQ files using fasterq-dump
+for sra_id in "${sra_ids[@]}"; do
+    echo "Downloading $sra_id with $threads threads..."
+    fasterq-dump --split-files --threads "$threads" --verbose "$sra_id"
+    gzip "${sra_id}_1.fastq" "${sra_id}_2.fastq"
+done
+
+echo "Download completed."

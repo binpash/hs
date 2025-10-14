df6a13b4da433447ae7398a117e7d6f1d9b72fa2
Bolun Thompson <bolunthompson@ucla.edu>
2025-10-14 15:27:43 -0700

    fix: docker priv for python hs

--- python_hs/report/benchmark.sh
+++ python_hs/report/benchmark.sh
@@ -10,6 +10,7 @@ cd "$HS_ROOT/python_hs/report"
 docker build -t hs "$HS_ROOT"
 docker build -t python-hs-benchmarks .
 docker run --rm \
+    --init --privileged --cgroupns=host \
     -v "$PWD/data:$DOCKER_ROOT/data" \
     python-hs-benchmarks \
     ./run_specific_benchmark.sh "$@"

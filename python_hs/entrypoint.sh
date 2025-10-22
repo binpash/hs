df1e4cd9b4f990b5edbc39e6530c19de8e159a22
Bolun Thompson <bolunthompson@ucla.edu>
2025-10-21 19:30:43 -0700

    fix: trap cleanup_server

--- python_hs/entrypoint.sh
+++ python_hs/entrypoint.sh
@@ -13,6 +13,7 @@ export PASH_TMP_PREFIX="$(mktemp -d /tmp/pash_XXXXXXX)/"
 source "$PASH_TOP/compiler/orchestrator_runtime/pash_init_setup.sh" --speculative
 # sets daemon_pid
 start_server "$@"
+trap "cleanup_server $daemon_pid" EXIT INT TERM
+
 python3 "$PASH_SPEC_TOP/python_hs/python_hs/entrypoint.py" "$FILE"
-cleanup_server "$daemon_pid"
 

06e89ca77877b01e9efd9246d817523b32536c72
Bolun Thompson <bolunthompson@ucla.edu>
2025-10-21 19:32:54 -0700

    add biostars script

---
+++ python_hs/report/benchmarks/biostars-multiprocessing/README.md
@@ -0,0 +1,8 @@
+Taken from https://www.biostars.org/p/174807/
+
+Changes made manually:
+- Inlined all the functions
+- Replaced subprocess.call with subprocess.run
+- Formatting
+- Hard coded inputs
+- Removed logically unnecessary += statements because the preprocessor doesn't support them
---
+++ python_hs/report/benchmarks/biostars-multiprocessing/script.py
@@ -0,0 +1,23 @@
+"""
+Simple subprocess.run in a loop example.
+
+Taken from https://www.biostars.org/p/174807/, with modifications.
+"""
+
+import os
+import subprocess
+
+Orig_file_dir = "data/biostars-multiprocessing/"
+NT_align_file_dir = "output/biostars-multiprocessing/NT_aligned"
+AA_align_file_dir = "output/biostars-multiprocessing/AA_aligned"
+
+subprocess.run(["mkdir", "-p", NT_align_file_dir])
+subprocess.run(["mkdir", "-p", AA_align_file_dir])
+
+for currentFile in os.listdir(Orig_file_dir):
+    if currentFile.endswith(".fa"):
+        input_file = Orig_file_dir + currentFile
+        NT_output_file = NT_align_file_dir + currentFile[:-3] + "_NT_aligned.fa"
+        AA_output_file = AA_align_file_dir + currentFile[:-3] + "_AA_aligned.fa"
+        MACSE_command = f"java -jar tools/macse_v1.01b.jar -prog alignSequences -seq {input_file} -out_NT {NT_output_file} -out_AA {AA_output_file}"
+        subprocess.run(MACSE_command, shell=True)

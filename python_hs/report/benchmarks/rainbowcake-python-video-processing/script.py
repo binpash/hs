2f020cd60fc972397e6eec2bae703ed2102cdbf0
Bolun Thompson <bolunthompson@ucla.edu>
2025-11-11 12:42:57 -0800

    feat: remove trim/duration limit from video processing bench

--- python_hs/report/benchmarks/rainbowcake-python-video-processing/script.py
+++ python_hs/report/benchmarks/rainbowcake-python-video-processing/script.py
@@ -7,11 +7,8 @@ output_dir = "./output/rainbowcake-python-video-processing/"
 
 image_path = os.path.join(data_dir, "watermark.png")
 
-duration = 10
-
 subprocess.run(["mkdir", "-p", output_dir], check=True)
 
-# loop is our only logical addition
 for video_name in glob.glob(f"{data_dir}/*.mkv"):
     basename = os.path.basename(video_name)
     output_name = f"processed_{basename}"
@@ -26,12 +23,10 @@ for video_name in glob.glob(f"{data_dir}/*.mkv"):
             video_name,
             "-i",
             image_path,
-            "-t",
-            f"{duration}",
             "-filter_complex",
-            "[0]trim=start_frame=0:end_frame=50[v0];\
-        [0]trim=start_frame=100:end_frame=150[v1];[v0][v1]concat=n=2[v2];[1]hflip[v3];\
-        [v2][v3]overlay=eof_action=repeat[v4];[v4]drawbox=50:50:120:120:red:t=5[v5]",
+            "[1]hflip[v3];\
+            [0][v3]overlay=eof_action=repeat[v4];\
+            [v4]drawbox=50:50:120:120:red:t=5[v5]",
             "-map",
             "[v5]",
             output_dir + output_name,

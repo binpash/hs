5d5fbdc73992f35ca350b7f631cabf348a3a48c1
Bolun Thompson <bolunthompson@ucla.edu>
2025-11-30 22:48:32 -0500

    refactor three runs to use makefile

--- python_hs/report/three_runs.sh
+++ python_hs/report/three_runs.sh
@@ -1,17 +1,3 @@
-mkdir -p grand_three_runs/
+#!/bin/bash
 
-# echo "Nemo Started!"
-# bash nemo.sh &> grand_three_runs/nemo_log
-# echo "Nemo Done!"
-
-echo "Biostars Started!"
-bash biostars.sh &> grand_three_runs/biostars_log
-echo "Biostars Done!"
-
-echo "Bioinfo Started!"
-bash bioinfo.sh &> grand_three_runs/bioinfo_log
-echo "Bioinfo Done!"
-
-# TODO: Bioinfo after
-# TODO: Rainbowcake after
-# TODO: Captk if time.
+make download-all all &> grand_final_log

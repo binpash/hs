d326f3a73c7c56a05eab0c0c440dae947e392063
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2026-02-06 14:42:40 -0800

    fix

--- jit_runtime/pash_declare_vars.sh
+++ jit_runtime/pash_declare_vars.sh
@@ -4,11 +4,6 @@ vars_file="${1?File not given}"
 
 # pash_redir_output echo "Writing vars to: $vars_file"
 
-echo "cd \"${PWD}\"" > "$vars_file"
+echo "cd ${PWD}" > "$vars_file"
 declare -p >> "$vars_file"
 declare -f >> "$vars_file"
-trap >> "$vars_file"
-echo "BASH_ARGV0=\"$0\"" >> "$vars_file"
-echo '${hs_set_options_cmd}' >> "$vars_file"
-echo 'set -- "${hs_runtime_tmp_args[@]}"' >> "$vars_file"
-${RUNTIME_LIBRARY_DIR}/fd_util -s -f "$vars_file.fds"

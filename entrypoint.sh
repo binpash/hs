62145a41e65a691d8a670cf41e0c4482b4355c6d
Dan Liu <nad2040@gmail.com>
2026-05-18 09:25:55 -0400

    new attempt: mount more fs and set memlock limit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 base=$(dirname $0)
 source ${base}/.venv/bin/activate
-mount -t bpf bpffs /sys/fs/bpf 2>/dev/null || true
 exec "$@"
--- report/run_benchmark.py
+++ report/run_benchmark.py
@@ -3,6 +3,7 @@
 import argparse
 from pathlib import Path
 import os
+import resource
 import time
 import shutil
 from subprocess import run, PIPE
@@ -128,6 +129,7 @@ def do_trace_v3_run(test_base: Path, output_base: Path, env: dict, script_name: 
     output_dir.mkdir(parents=True, exist_ok=True)
     env['OUTPUT_DIR'] = str(output_dir)
 
+    resource.setrlimit(resource.RLIMIT_MEMLOCK, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
     try:
         run(['trace_v3', 'install'], check=False)
     except FileNotFoundError:
